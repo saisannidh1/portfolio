3b0f57342d3e49ee9b4d0a4699128fcd17015bf6
HugoMonteiro12 <hugondarez@gmail.com>
2024-10-31 16:33:59 +0000

    V1.0

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const darkModeToggle = document.getElementById('darkModeToggle');
 
+function toggleDarkMode() {
+  document.body.classList.toggle('dark-mode');
+}
 darkModeToggle.addEventListener('click', () => {
   document.body.classList.toggle('dark-mode');
 });
