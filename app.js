de2b02f9858e3227f48e5658421f452a0f328284
HugoMonteiro12 <hugondarez@gmail.com>
2024-10-31 19:14:31 +0000

    V1.0

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
-//toggle dark mode
 function toggleDarkMode() {
   document.body.classList.toggle('dark-mode');
-
   const toggleButton = document.getElementById('darkModeToggle');
   const icon = toggleButton.querySelector('i');
 
   if(document.body.classList.contains('dark-mode')) {
-    icon.classList.replace('fa-moon', 'fa-sun');  // Changed this line
+    icon.classList.replace('fa-moon', 'fa-sun');
     localStorage.setItem('darkMode', 'enabled');
   } else {
-    icon.classList.replace('fa-sun', 'fa-moon');  // Changed this line
+    icon.classList.replace('fa-sun', 'fa-moon');
     localStorage.setItem('darkMode', 'disabled');
   }
 }
 
-//check for saved dark mode preference
 document.addEventListener('DOMContentLoaded', () => {
   const darkModePreference = localStorage.getItem('darkMode');
   const toggleButton = document.getElementById('darkModeToggle');
@@ -22,16 +19,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
   if (darkModePreference === 'enabled') {
     document.body.classList.add('dark-mode');
-    icon.classList.replace('fa-moon', 'fa-sun');  // Changed this line
+    icon.classList.replace('fa-moon', 'fa-sun');
   }
 
-  //listener for the button
   toggleButton.addEventListener('click', toggleDarkMode);
-});
-
-// app.js
-
-document.getElementById('mobile-menu').addEventListener('click', function() {
-  const nav = document.querySelector('.nav');
-  nav.classList.toggle('active'); // Toggle the active class
 });
