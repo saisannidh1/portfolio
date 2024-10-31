c39eeae6688a5d3b7a26d5da8898c50474c4266c
HugoMonteiro12 <hugondarez@gmail.com>
2024-10-31 16:39:12 +0000

    V1.0

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
-const darkModeToggle = document.getElementById('darkModeToggle');
-
 function toggleDarkMode() {
   document.body.classList.toggle('dark-mode');
+
+  const toggleButton = document.getElementById('darkModeToggle');
+  if(document.body.classList.contains('dark-mode')) {
+    toggleButton.textContent = 'Enable Light Mode';
+    localStorage.setItem('darkMode', 'enabled');
+  } else {
+    toggleButton.textContent = 'Enable Dark Mode';
+    localStorage.setItem('darkMode', 'disabled');
+  }
 }
-darkModeToggle.addEventListener('click', () => {
-  document.body.classList.toggle('dark-mode');
+
+document.addEventListener('DOMContentLoaded', () => {
+  const darkModePreference = localStorage.getItem('darkMode');
+  const toggleButton = document.getElementById('darkModeToggle');
+
+  if (darkModePreference === 'disabled') {
+    document.body.classList.remove('dark-mode');
+    toggleButton.textContent = 'Enable Dark Mode';
+  } else {
+    toggleButton.textContent = 'Enable Light Mode';
+  }
 });
